f1ce4daddf9be21af5128942ffef5264a873be35
kante24 <kantedavid@gmail.com>
2021-10-29 11:12:25 -0400

    Debut du projet

---
+++ app/src/main/java/com/david/aliensolarsystem/DebutJeu.java
@@ -0,0 +1,14 @@
+package com.david.aliensolarsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DebutJeu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debut_jeu);
+    }
+}
